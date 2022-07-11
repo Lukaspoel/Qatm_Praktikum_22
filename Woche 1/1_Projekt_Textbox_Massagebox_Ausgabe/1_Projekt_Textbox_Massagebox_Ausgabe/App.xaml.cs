eda94d4f3d65c2cfe6e8691e4b380f17b4e4294e
Lukas <Poelzleitner.Lukas@qness.com>
2022-07-11 14:38:24 +0200

    Textbox integriert - Inhalt wird ausgegeben

---
+++ Woche 1/1_Projekt_Textbox_Massagebox_Ausgabe/1_Projekt_Textbox_Massagebox_Ausgabe/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _1_Projekt_Textbox_Massagebox_Ausgabe
+{
+    /// <summary>
+    /// Interaktionslogik für "App.xaml"
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ Woche 1/1_Projekt_Textbox_Massagebox_Ausgabe/1_Projekt_Textbox_Massagebox_Ausgabe/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _1_Projekt_Textbox_Massagebox_Ausgabe
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void txtAusgabe_TextChanged(object sender, TextChangedEventArgs e)  
+        {
+
+        }
+
+        private void KlickHere_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(txtInputOutput.Text);
+
+        }
+    }
+}
